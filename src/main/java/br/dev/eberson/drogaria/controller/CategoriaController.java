62083cdb01a8dbb9b0efd6f44594a819299fd37a
Eber <eberson.santoscosme@gmail.com>
2024-09-19 11:53:22 -0300

    Metodo de update

--- src/main/java/br/dev/eberson/drogaria/controller/CategoriaController.java
+++ src/main/java/br/dev/eberson/drogaria/controller/CategoriaController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,5 +38,10 @@ public class CategoriaController {
 		Optional<Categoria> categoria = categoriaRepository.findById(codigo);
 		categoriaRepository.delete(categoria.get());
 		return categoria.get();
+	}
+	
+	@PutMapping
+	public Categoria editar(@RequestBody Categoria categoria) {
+		return categoriaRepository.save(categoria);
 	}
 }
