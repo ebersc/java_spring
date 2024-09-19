7139a9c0a8e2e1f24ed07029d9aa670720fb091b
Eber <eberson.santoscosme@gmail.com>
2024-09-19 11:48:51 -0300

    Metodo de exclusao

--- src/main/java/br/dev/eberson/drogaria/controller/CategoriaController.java
+++ src/main/java/br/dev/eberson/drogaria/controller/CategoriaController.java
@@ -1,9 +1,12 @@
 package br.dev.eberson.drogaria.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,5 +30,12 @@ public class CategoriaController {
 	@PostMapping
 	public Categoria inserir(@RequestBody Categoria categoria) {
 		return categoriaRepository.save(categoria);
+	}
+	
+	@DeleteMapping("/{codigo}")
+	public Categoria excluir(@PathVariable Byte codigo) {
+		Optional<Categoria> categoria = categoriaRepository.findById(codigo);
+		categoriaRepository.delete(categoria.get());
+		return categoria.get();
 	}
 }
